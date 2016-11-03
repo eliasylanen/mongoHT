310d239f908f536735bccd553fa95b181d777c20
Elias Ylänen <elias.ylanen@gmail.com>
2016-11-03 11:03:26 +0200

    kikkeliskokkelis

--- app/client/script/fetch-demo.js
+++ app/client/script/fetch-demo.js
@@ -4,24 +4,37 @@ fetch('/langs')
   .then(data => data.json())
   .then(test => {
     testdata = test.msg;
+    const langWrap = document.getElementById('lang-wrap');
     for (const data of testdata) {
       const langLink = document.createElement('BUTTON');
-      const langWrap = document.getElementById('lang-wrap');
       // langLink = langLink.innerHTML += `${data._id}(${data.count}) `;
       const langLinkContent = document.createTextNode(`${data._id}(${data.count}) `);
       langLink.value = data._id.toLowerCase();
+      langLink.className = 'lang';
       langLink.appendChild(langLinkContent);
       langWrap.appendChild(langLink);
     }
+    const headerWrapper = document.createElement('div');
+    langWrap.appendChild(headerWrapper);
+    fetchLang(headerWrapper);
   });
 
-const links = document.getElementsByClassName('lang');
-Array.prototype.forEach.call(links, (link) => {
-  link.addEventListener('click', (e) => {
-    fetch('/langs/e.target.value')
-      .then(data => data.json())
-      .then(test => {
-        console.log(test.msg);
-      });
+function fetchLang(headerWrapper) {
+  const links = document.getElementsByClassName('lang');
+  Array.prototype.forEach.call(links, (link) => {
+    link.addEventListener('click', (e) => {
+      headerWrapper.innerHTML = '';
+      fetch(`/langs/${e.target.value}`)
+        .then(data => data.json())
+        .then(test => {
+          for (const data of test.msg) {
+            const poemDiv = document.createElement('div');
+            const poemH = document.createElement('h1');
+            poemH.textContent = `${data.title}`;
+            poemDiv.appendChild(poemH);
+            headerWrapper.appendChild(poemDiv);
+          }
+        });
+    });
   });
-});
+}
