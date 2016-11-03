17ac30f706d1a06a38ea5cec4edadaa3138a3d33
Juuso Vuorinen <Juuso Vuorinen>
2016-11-03 10:22:44 +0200

    tas olis markku

--- app/client/script/fetch-demo.js
+++ app/client/script/fetch-demo.js
@@ -5,6 +5,12 @@ fetch('/langs')
   .then(test => {
     testdata = test.msg;
     for (const data of testdata) {
-      document.getElementById('test').innerHTML += `${data._id}(${data.count}) `;
+      var langLink = document.createElement('BUTTON');
+      var langWrap = document.getElementById('lang-wrap');
+      // langLink = langLink.innerHTML += `${data._id}(${data.count}) `;
+      var langLinkContent = document.createTextNode(`${data._id}(${data.count}) `);
+      langLink.value = data._id.toLowerCase();
+      langLink.appendChild(langLinkContent);
+      langWrap.appendChild(langLink);
     }
   });
