d76c44f5a91db5c81037ef08e849a3126915346b
Elias Ylänen <elias.ylanen@gmail.com>
2016-11-03 10:26:55 +0200

    jokucommitti message

--- app/client/script/fetch-demo.js
+++ app/client/script/fetch-demo.js
@@ -5,6 +5,17 @@ fetch('/langs')
   .then(test => {
     testdata = test.msg;
     for (const data of testdata) {
-      document.getElementById('test').innerHTML += `${data._id}(${data.count}) `;
+      document.getElementById('test').innerHTML += `${data._id} `;
     }
   });
+
+const links = document.getElementsByClassName('lang');
+Array.prototype.forEach.call(links, (link) => {
+  link.addEventListener('click', (e) => {
+    fetch('/langs/e.target.value')
+      .then(data => data.json())
+      .then(test => {
+        console.log(test.msg);
+      });
+  });
+});
